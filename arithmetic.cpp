9dc38b50a6bcd4424edf1dfecb9caea543e85a69
DinDin03 <dineth.himasha@outlook.com>
2023-03-08 01:04:07 +1030

    Adding empty cpp files

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -1,1 +1,29 @@
-//Empty files
+#include <iostream>
+using namespace std;
+
+void arithmetic(int array[], int secondarray[])
+{
+    int result[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int carry[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    for (int i = 0; i < 8; i++)
+    {
+        if (array[i] == 1 && secondarray[i] == 1)
+        {
+            result[i] = 0;
+            carry[i + 1] = 1;
+        }
+        else
+        {
+            result[i] = array[i] + secondarray[i] + carry[i];
+        }
+        std::cout << result[i] << std::endl;
+    }
+}
+
+int main()
+{
+    int array[8] = {0, 0, 0, 0, 0, 0, 0, 1};
+    int secondarray[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    arithmetic(array, secondarray);
+    return 0;
+}
