91875229f3774edfcdd8877a06a75d8f154924ff
DinDin03 <dineth.himasha@outlook.com>
2023-03-09 15:25:14 +1030

    adding files

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,1 +1,10 @@
-// empty
+#include <iostream>
+using namespace std;
+
+extern int sum_diagonal(int[4][4]);
+
+int main(){
+    int array[4][4] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    std::cout << "The diagonal sum is: " << sum_diagonal(array) << std::endl;
+    return 0;
+}
