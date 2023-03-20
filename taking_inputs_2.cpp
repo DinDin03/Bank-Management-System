3bc2feb037db81ba6b9fe2730189fd2180653040
DinDin03 <dineth.himasha@outlook.com>
2023-03-20 22:08:51 +1030

    adding files

---
+++ taking_inputs_2.cpp
@@ -0,0 +1,60 @@
+#include <math.h>
+
+#include <iostream>
+using namespace std;
+
+void check(int *ptr){
+    int num = *ptr;
+    cout << "The value pointing at this address is: "<< num << endl;
+}
+
+int binary_to_decimal(int num, int binary[]) {
+  int index = 0, decimal = 0, *ptr;
+  for (int i = num - 1; i >= 0; i--) {
+    decimal += binary[i] * pow(2, index);
+    index++;
+  }
+  ptr = &decimal;
+  cout << "The decimal representation of the binary number is: " << decimal
+       << endl;
+  check(ptr);
+  return *ptr;
+}
+
+int userinput(int num) {
+  int binary[num];
+  int digit;
+  for (int i = 0; i < num; i++) {
+    cout << "Enter the digit: ";
+    cin >> digit;
+    if (digit != 1 && digit != 0) {
+      while (digit != 0 && digit != 1) {
+        cout << "Invalid number. Enter the digit: ";
+        cin >> digit;
+        binary[i] = digit;
+      }
+    } else
+      binary[i] = digit;
+  }
+  cout << "The binary number you entered was: ";
+  for (int i = 0; i < num; i++) {
+    cout << binary[i];
+  }
+  cout << endl;
+  binary_to_decimal(num, binary);
+  //binary_to_hexadecimal(decimal, binary);
+  return *binary;
+}
+
+int main() {
+  int num;
+  cout << "How many binary digits are there: ";
+  cin >> num;
+  if (num <= 0) {
+    while (num <= 0) {
+      cout << "Invalid number. Enter again: ";
+      cin >> num;
+    }
+  }
+  userinput(num);
+}
