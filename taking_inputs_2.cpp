e5d50f1de6551887f0ce47e083e03018ef550f81
DinDin03 <dineth.himasha@outlook.com>
2023-03-20 23:32:12 +1030

    adding files

--- taking_inputs_2.cpp
+++ taking_inputs_2.cpp
@@ -1,17 +1,51 @@
 #include <math.h>
+#include <string.h>
 #include <iostream>
 using namespace std;
 
-void decimal_to_hexadecimal(int *ptr){
-    int decimal = *ptr;
-    int decimal2 = decimal;
-    int rem = decimal/16;
-    int mod;
-    for(int i = 0;i<rem;i++){
-      decimal2 = decimal
-      mod = decimal%16;
+void decimal_to_hexadecimal(int *ptr) {
+  int decimal = *ptr;
+  int decimal2 = decimal;
+  int mod, count = 0;
+  char hex[30];
+  while (decimal2 > 0) {
+    mod = decimal2 % 16;
+    cout << "Current decimal: " << decimal2 << endl;
+    cout << "Current mod: " << mod << endl;
+    cout << "Current count: " << count << endl;
+    if (mod >= 0 && mod <= 9) {
+      hex[count] = mod;
     }
+    else
+    switch(mod){
+      case 10:
+        hex[count] = 'A';
+        break;
+      case 11:
+        hex[count] = 'B';
+        break;
+      case 12:
+        hex[count] = 'C';
+        break;
+      case 13:
+        hex[count] = 'D';
+        break;
+      case 14:
+        hex[count] = 'E';
+        break;
+      case 15:
+        hex[count] = 'F';
+        break;
+    }
+    count++;
+    decimal2 = decimal2 / 16;
     
+  }
+  cout << "The Hexadecimal representation of the binary number you entered is: ";
+  for (int i = count-1; i>=0; i--) {
+    cout << hex[i];
+  }
+  cout  << endl;
 }
 
 int binary_to_decimal(int num, int binary[]) {
@@ -23,7 +57,7 @@ int binary_to_decimal(int num, int binary[]) {
   ptr = &decimal;
   cout << "The decimal representation of the binary number is: " << decimal
        << endl;
-  check(ptr);
+  decimal_to_hexadecimal(ptr);
   return *ptr;
 }
 
@@ -48,7 +82,7 @@ int userinput(int num) {
   }
   cout << endl;
   binary_to_decimal(num, binary);
-  //binary_to_hexadecimal(decimal, binary);
+  // binary_to_hexadecimal(decimal, binary);
   return *binary;
 }
 
