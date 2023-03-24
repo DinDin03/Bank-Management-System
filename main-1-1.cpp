4f9d9edd0b187f6495b6a371242b48621b88234d
DinDin03 <dineth.himasha@outlook.com>
2023-03-24 10:30:31 +1030

    adding files

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,13 +3,10 @@
 #include <string>
 using namespace std;
 
-extern Person createPersonArray(int);
+extern Person* createPersonArray(int);
 
 int main(){
 
-    Person p1;
-    p1.name = "John Doe";
-    p1.age = 0;
     createPersonArray(5);
     return 0;
 }
