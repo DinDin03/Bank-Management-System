42c0bdfc9c22cb86ac5dae9cbacf7358015401ec
Dineth Katanwala <dindin@DinDIN.localdomain>
2023-03-24 19:08:27 +1030

    adding files

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -16,15 +16,15 @@ PersonList createPersonList(int n){
 }
 
 PersonList deepCopyPersonList(PersonList pl){
-    PersonList pList;
-    pList.numPeople = pl.numPeople;
-    Person* newarray = new Person[pl.numPeople];
+    PersonList pList2;
+    pList2.numPeople = pl.numPeople;
+    Person* newarray = new Person[pList2.numPeople];
     Person* oldArray;
-    pList.people = newarray;
-    pl.people = oldArray;
+    pList2.people = newarray;
+    oldArray = pl.people;
     for(int i = 0; i < pl.numPeople; i++){
         newarray[i].name = oldArray[i].name;
         newarray[i].age = oldArray[i].age;
     }
-    return pList;
+    return pList2;
 }
