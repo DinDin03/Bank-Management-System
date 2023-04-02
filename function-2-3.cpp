02db66ee8f4227e2c3f87a50a3e20c4b4276c29e
DinDin03 <dineth.himasha@outlook.com>
2023-04-03 04:40:13 +0930

    adding files

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,49 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int sum_if_palindrome(int integers[], int length){
-    if(length <= 0){
+bool is_palindrome(int integers[], int length) {
+    if (length <= 0) {
         return false;
     }
-    int sum = 0;
-    for(int i = 0; i<length; i++){
-        sum += integers[i];
-    }
-    std::cout << sum << std::endl;
-    return 0;
-}
-
-int sum_array_elements(int integers[], int length){
-    if(length <= 0){
-        return false;
-    }
-    int sum2 = 0;
-    for(int i = 0; i<length; i++){
-        sum2 += integers[i];
-    }
-    std::cout << sum2 << std::endl;
-    return 0;
-}
-
-bool is_palindrome(int integers[], int length)
-{
-    if(length <= 0){
-        return false;
-    }
-    int ispalindrome = 0;
-    for (int i = 0; i < length / 2 && length != 0; i++)
-    {
-        if(integers[i] != integers[length-1-i]){
-            ispalindrome = 1;
-            break;
+    for (int i = 0; i < length / 2; i++) {
+        if (integers[i] != integers[length - i - 1]) {
+            return false;
         }
     }
-    if(ispalindrome == 1){
-        sum_array_elements(integers,length);
+    return true;
+}
+
+int sum_array_elements(int integers[], int length) {
+    if (length <= 0) {
+        return -1;
+    }
+    int sum = 0;
+    for (int i = 0; i < length; i++) {
+        sum += integers[i];
+    }
+    return sum;
+}
+
+int sum_if_palindrome(int integers[], int length) {
+    if (!is_palindrome(integers, length)) {
         return -2;
     }
-    else
-        sum_if_palindrome(integers,length);
-        return 0;
+    return sum_array_elements(integers, length);
 }
+
 
--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
-extern bool is_palindrome(int[], int);
-int main(){
-    int array[4] = {1,0,1,1};
-    int size = sizeof(array)/sizeof(int);
-    std::cout << is_palindrome(array,size) << std::endl;
+extern bool sum_if_palindrome(int[], int);
+int main() {
+    int arr1[] = {1, 2, 3, 4, 3, 2, 1};
+    int arr2[] = {1, 2, 3, 4, 5, 6};
+    int arr3[] = {};
+    int arr4[] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+
+    cout << "arr1 sum: " << sum_if_palindrome(arr1, 7) << endl; // prints "arr1 sum: 16"
+    cout << "arr2 sum: " << sum_if_palindrome(arr2, 6) << endl; // prints "arr2 sum: -2"
+    cout << "arr3 sum: " << sum_if_palindrome(arr3, 0) << endl; // prints "arr3 sum: -1"
+    cout << "arr4 sum: " << sum_if_palindrome(arr4, 9) << endl; // prints "arr4 sum: 20"
+
     return 0;
 }
