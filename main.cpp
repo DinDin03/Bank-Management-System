4ebb8577d719c9ade9d7574bad108eecd9d81b51
DinDin03 <dineth.himasha@outlook.com>
2023-06-19 02:15:45 +0930

    addin files

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 
 // The `extern` keyword tells the compiler that the count function exists,
 // but will be implemented somewhere else
-extern int count(int[], int);
+
 
 int main() {
+    int count(int[], int);
     int array[5] = {4,5,6,7,8};
     std::cout << "The number is: " << count(array, 5) << std::endl;
     return 0;
